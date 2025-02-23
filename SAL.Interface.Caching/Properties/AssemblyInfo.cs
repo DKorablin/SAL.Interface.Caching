e2bee8a15bc4108442392447fe94020b7e250b7a
DKorablin2 <danila.korablin@unity3d.com>
2025-02-24 01:45:08 +0300

    Unify plugins build

--- SAL.Interface.Caching/Properties/AssemblyInfo.cs
+++ SAL.Interface.Caching/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("c10f2c0a-f027-4ba7-9cf4-aa21c96120ad")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://github.com/DKorablin/SAL.Interface.Caching")]
 #else
 
-[assembly: AssemblyTitle("Interface.Caching")]
 [assembly: AssemblyDescription("Base SAL interfaces for caching plugins")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Interface.Caching")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2024")]
 #endif
