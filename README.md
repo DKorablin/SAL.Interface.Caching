1c1c4653add2a456f1a2dfbb098b4b523c30234d
DKorablin2 <danila.korablin@unity3d.com>
2025-11-04 01:32:45 +0300

    Updated csproj format and merged with nuspec file Added support for .NET Standard 2.0 Added CPM Updated SAL BCL

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # SAL caching interfaces
+[![Auto build](https://github.com/DKorablin/SAL.Interface.Caching/actions/workflows/release.yml/badge.svg)](https://github.com/DKorablin/SAL.Interface.Caching/releases/latest)
+
 Base interfaces for caching logic used inside SAL plugins
--- SAL.Interface.Caching/Properties/AssemblyInfo.cs
+++ SAL.Interface.Caching/Properties/AssemblyInfo.cs
@@ -4,10 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("c10f2c0a-f027-4ba7-9cf4-aa21c96120ad")]
 [assembly: System.CLSCompliant(true)]
 
-#if NETCOREAPP
-[assembly: AssemblyMetadata("ProjectUrl", "https://github.com/DKorablin/SAL.Interface.Caching")]
-#else
-
 [assembly: AssemblyDescription("Base SAL interfaces for caching plugins")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2024")]
-#endif
