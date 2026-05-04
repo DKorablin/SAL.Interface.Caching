43411d2b369990d4d815c034a54558ce411ac3d9
DKorablin <4739804+DKorablin@users.noreply.github.com>
2026-05-04 05:41:47 +0300

    Refactored Plugin class to use dependency injection for ITraceSource instead of internal TraceSource logic. Updated constructor to require both IHost and ITraceSource. Removed CreateTraceSource method. Bumped SAL.Flatbed NuGet package to 1.2.12.

--- SAL.Interface.Caching/Properties/AssemblyInfo.cs
+++ SAL.Interface.Caching/Properties/AssemblyInfo.cs
@@ -5,4 +5,3 @@ using System.Runtime.InteropServices;
 [assembly: System.CLSCompliant(true)]
 
 [assembly: AssemblyDescription("Base SAL interfaces for caching plugins")]
-[assembly: AssemblyCopyright("Copyright © Danila Korablin 2016-2025")]
